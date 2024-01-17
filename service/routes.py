7d0890303060cce37d91f913ab004fc5ada923d6
MasonCathorall <116600072+MasonCathorall@users.noreply.github.com>
2024-01-17 01:33:11 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -3,7 +3,6 @@ Account Service
 
 This microservice handles the lifecycle of Accounts
 """
-pylint: disable=unused-import
 from flask import jsonify, request, make_response, abort  # noqa; F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
